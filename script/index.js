33b279de84fa51c1a2b5bcfc20082514361ae0cb
Ramesh Raman <ramesh18.r@gmail.com>
2021-12-24 15:42:35 -0600

    refactored the code. wrote functions to reuse

--- script/index.js
+++ script/index.js
@@ -5,13 +5,13 @@ function subtract(a, b) {
     return (a - b);
 }
 function multiply(a, b) {
-    return a * b;
+    return +(a * b).toFixed(6);
 }
 function divide(a, b) {
     if (b === 0) {
         return "u shud noe betta";
     }
-    return a / b;
+    return +(a / b).toFixed(6);
 }
 
 function operate(num1, num2, operator) {
@@ -28,9 +28,12 @@ function operate(num1, num2, operator) {
     }
 }
 
+
 let results = document.querySelector(".result");
 let backspace = document.querySelector(".operator-backspace");
 const buttons = document.querySelectorAll('button');
+//DISABLED BUTTONS
+document.querySelector(".operator-signed").disabled = true;
 
 let scrapArr = [];
 let number1 = '';
@@ -44,165 +47,116 @@ const operatorsSymbol = ['=', '+', '-', '/', '*', 'Enter'];
 const allAllowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '/', '*', 'Backspace', 'Enter'];
 
 
-//DISABLED BUTTONS
-document.querySelector(".operator-signed").disabled = true;
+function acceptNumber(key) {
+    scrapArr.push(key);
+    results.textContent = scrapArr.join('');
+    prevButtonisOperator = false;
+    previousButtonPressed = ''; //reset the previous button since user pressed a number.
+}
 
-//BEGIN KEYDOWN SELECTORS
+function clearEntry(Arrlength, prevButton) {
+    if (Arrlength > 0 && prevButton == false) {
+        scrapArr.pop();
+        results.textContent = scrapArr.join('');
+    }
+    else if (Arrlength == 0 && prevButton == true) {
+        operator = '';
+        results.textContent = results.textContent.substring(0, results.textContent.length - 1);
+        prevButtonisOperator = false;
+    }
+    else if (Arrlength == 0 && prevButton == false) {
+        return; //do nothing
+    }
+}
+
+function acceptOperator(key) {
+    if (number1.length == 0) { //will only happen first time
+        number1 = scrapArr.join('');
+    } else { //will happen always except first time
+        number2 = scrapArr.join('');
+    }
+
+    scrapArr = [];
+
+    if (number1.length !== 0 && number2.length !== 0 && operator !== '') {
+        finalVal = operate(number1, number2, operator);
+        results.textContent = finalVal; //printing to UI
+        number1 = finalVal;
+        number2 = '';
+    }
+
+    //update the operator
+    if (key === "=" || key === "Enter") {
+        operator = '';
+        previousButtonPressed = "="; //assign previous button as =
+    }
+    else {
+        operator = key;
+        results.textContent += operator; //print operator to the UI
+        prevButtonisOperator = true;
+        previousButtonPressed = ''; //reset the previous button since user pressed a true operator.
+    }
+}
+
+function clearScreen() {
+    results.textContent = '';
+    number1 = '';
+    number2 = '';
+    operator = '';
+    scrapArr = []
+    prevButtonisOperator = false;
+    previousButtonPressed = '';
+}
+
+
+//Begin keypress selector
 window.addEventListener('keydown', (e) => {
     if (!allAllowedKeys.includes(e.key)) { return; } //if the key pressed isn't in the calculator, do nothing
 
-    //NUMBER ENTERED BEGIN
+    //Process numbers
     if (!operatorsSymbol.includes(e.key) && e.key != 'Backspace') {
-        if (e.key == "." && scrapArr.includes(".")) {
-            return;
-        }
-        //user shouldn't enter a number following =. They can either clear screen or continue calculation by
-        //  entering an operator following =. 1+2 = 3 ... / 3 and so on.
+        if (e.key == "." && scrapArr.includes(".")) { return; }
+        //user shouldn't enter a number following =. They can either clear screen or continue calculation entering a operator
         if (previousButtonPressed == "=") { return; }
+        else { acceptNumber(e.key); }
+    }
 
-        else {
-            scrapArr.push(e.key);
-            results.textContent = scrapArr.join('');
-            prevButtonisOperator = false;
-            previousButtonPressed = ''; //reset the previous button since user pressed a number.
-            return;
-        }
+    //clear previous entry
+    if (e.key == 'Backspace') {
+        clearEntry(scrapArr.length, prevButtonisOperator);
     }
-    //END NUMBER ENTERED
 
-    //BEGIN CLEAR ENTRY OPERATOR
-    //clear previous number
-    if (e.key == 'Backspace' && scrapArr.length > 0 && prevButtonisOperator == false) {
-        scrapArr.pop();
-        results.textContent = scrapArr.join('');
-        return;
-    }
-    else if (e.key == 'Backspace' && scrapArr.length == 0 && prevButtonisOperator == false) {
-        return; //do nothing
-    }
-    //clear previous operator
-    else if (e.key == 'Backspace' && prevButtonisOperator == true) {
-        operator = '';
-        results.textContent = results.textContent.substring(0, results.textContent.length - 1);
-        prevButtonisOperator = false;
-        return;
-    }
-    //END CLEAR ENTRY OPERATOR
-
-    //BEGIN OPERATOR ENTERED
+    //Process Operators
     if (operatorsSymbol.includes(e.key) && prevButtonisOperator == false) {
-        if (number1.length == 0) { //will only happen first time
-            number1 = scrapArr.join('');
-        } else { //will happen always except first time
-            number2 = scrapArr.join('');
-        }
-
-        scrapArr = [];
-
-        if (number1.length !== 0 && number2.length !== 0 && operator !== '') {
-            finalVal = operate(number1, number2, operator);
-            results.textContent = finalVal; //printing to UI
-            number1 = finalVal;
-            number2 = '';
-        }
-
-        //update the operator
-        if (e.key == 'Enter') {
-            operator = '';
-            previousButtonPressed = "="; //assign previous button as =
-        }
-        else {
-            operator = e.key;
-            results.textContent += operator; //print operator to the UI
-            prevButtonisOperator = true;
-            previousButtonPressed = ''; //reset the previous button since user pressed a real operator(not =).
-        }
+        acceptOperator(e.key);
     }
-});
+}); //end keypress selector
 
 //BEGIN MOUSE CLICK LISTENER FUNCTION
 buttons.forEach(button => button.addEventListener('click', (e) => {
-    //BEGIN CLEAR SCREEN
+    //ALL CLEAR
     if (e.target.outerText === 'AC') {
-        results.textContent = '';
-        number1 = '';
-        number2 = '';
-        operator = '';
-        scrapArr = []
-        prevButtonisOperator = false;
-        previousButtonPressed = ''; 
+        clearScreen();
         return;
     }
-    //END CLEAR SCREEN
 
-    //BEGIN CLEAR ENTRY OPERATOR
-    //clear previous number
-    if (e.target.outerText === 'C' && scrapArr.length > 0 && prevButtonisOperator == false) {
-        scrapArr.pop();
-        results.textContent = scrapArr.join('');
+    //CLEAR PREVIOUS ENTRY
+    if (e.target.outerText === 'C') {
+        clearEntry(scrapArr.length, prevButtonisOperator);
     }
-    else if (e.target.outerText === 'C' && scrapArr.length == 0 && prevButtonisOperator == false) {
-        return; //do nothing
-    }
-    //clear previous operator
-    else if (e.target.outerText === 'C' && prevButtonisOperator == true) {
-        operator = '';
-        results.textContent = results.textContent.substring(0, results.textContent.length - 1);
-        prevButtonisOperator = false;
-        return;
-    }
-    //END CLEAR ENTRY OPERATOR
 
-
-    //BEGIN REGULAR OPERATOR ENTERED
+    //Process operators
     else {
-        //OPERATOR ENTERED +-/*=
         if (operatorsSymbol.includes(e.target.outerText) && prevButtonisOperator == false) {
-            if (number1.length == 0) { //will only happen first time
-                number1 = scrapArr.join('');
-            } else { //will happen always except first time
-                number2 = scrapArr.join('');
-            }
-
-            scrapArr = [];
-
-            if (number1.length !== 0 && number2.length !== 0 && operator !== '') {
-                finalVal = operate(number1, number2, operator);
-                results.textContent = finalVal; //printing to UI
-                number1 = finalVal;
-                number2 = '';
-            }
-
-            //update the operator
-            if (e.target.outerText === "=") {
-                operator = '';
-                previousButtonPressed = "="; //assign previous button as =
-            }
-            else {
-                operator = e.target.outerText;
-                results.textContent += operator; //print operator to the UI
-                prevButtonisOperator = true;
-                previousButtonPressed = ''; //reset the previous button since user pressed a true operator.
-            }
+            acceptOperator(e.target.outerText);
         }
-        //END REGULAR OPERATOR ENTERED
 
-        //NUMBER ENTERED BEGIN
+        //Process numbers
         else if (!operatorsSymbol.includes(e.target.outerText)) {
-            if (e.target.outerText == "." && scrapArr.includes(".")) {
-                return;
-            }
-
-            //user shouldn't enter a number following =. They can either clear screen or continue calculation by
-            //  entering an operator following =. 1+2 = 3 ... / 3 and so on.
+            if (e.target.outerText == "." && scrapArr.includes(".")) { return; }
+            //user shouldn't enter a number following =. They can either clear screen or continue calculation with operator
             if (previousButtonPressed == "=") { return; }
-
-            scrapArr.push(e.target.outerText);
-            results.textContent = scrapArr.join('');
-            prevButtonisOperator = false;
-            previousButtonPressed = ''; //reset the previous button since user pressed a number.
+            else { acceptNumber(e.target.outerText) };
         }
-        //END NUMBER ENTERED 
     }
-}));
-//END MOUSE CLICK LISTENER FUNCTION
+})); //END MOUSE CLICK LISTENER FUNCTION
